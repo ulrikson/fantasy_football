bf7cfffe6b4d2c4224e90377b86385b778856e15
ulrikson <erik.ulrikson@gmail.com>
2022-03-20 10:31:52 +0100

    improve

--- FPL.py
+++ FPL.py
@@ -12,23 +12,40 @@ class FPL:
 
         self.must_be_higher_than_zero = ['value_season', 'form', 'minutes']
 
-    def getPlayerDF(self):
-        slim_elements_df = self.elements_df[['second_name', 'team', 'element_type', 'selected_by_percent',
-                                             'now_cost', 'minutes', 'transfers_in', 'value_season', 'total_points', 'form']]
+    def getPlayerDf(self):
+        df = self.elements_df[['second_name', 'team', 'element_type',
+                               'now_cost', 'minutes', 'value_season', 'total_points', 'form']]
 
-        slim_elements_df['position'] = slim_elements_df.element_type.map(
+        df['position'] = df.element_type.map(
             self.elements_types_df.set_index('id').singular_name)
 
-        slim_elements_df['team'] = slim_elements_df.team.map(
+        df['team'] = df.team.map(
             self.teams_df.set_index('id').name)
 
-        slim_elements_df = self.removeZeroValues(slim_elements_df)
+        df = self.removeZeroValues(df)
 
-        return slim_elements_df
-    
+        return df
+
     def removeZeroValues(self, df):
         for column in self.must_be_higher_than_zero:
             df[column] = df[column].astype(float)
             df = df.loc[df[column] > 0]
+
+        return df
+
+    def createPivot(self, index, value):
+        df = self.getPlayerDf()
+
+        pivot = df.pivot_table(index=index, values=value,
+                               aggfunc=np.mean).reset_index()
+
+        pivot = pivot.sort_values(value, ascending=False)
+
+        return pivot
+
+    def dfPerPosition(self, position, sort_by):
+        df = self.getPlayerDf()
+        df = df.loc[df.position == position]
+        df = df.sort_values(sort_by, ascending=False)
 
         return df
