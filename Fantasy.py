c370a31444f9a69ccaf21f1ae18e3f5b48d8a290
ulrikson <erik.ulrikson@gmail.com>
2022-04-02 14:58:33 +0200

    max_cost

--- Fantasy.py
+++ Fantasy.py
@@ -3,30 +3,49 @@ import numpy as np
 
 
 class Fantasy:
-    def __init__(self, json, unwanted_teams, higher_than):
+    def __init__(self, json, unwanted_teams, higher_than, max_cost):
         self.json = json
 
-        self.elements_df = pd.DataFrame(json['elements'])
-        self.elements_types_df = pd.DataFrame(json['element_types'])
-        self.teams_df = pd.DataFrame(json['teams'])
+        self.elements_df = pd.DataFrame(json["elements"])
+        self.elements_types_df = pd.DataFrame(json["element_types"])
+        self.teams_df = pd.DataFrame(json["teams"])
 
         self.unwanted_teams = unwanted_teams
         self.higher_than = higher_than
+        self.max_cost = max_cost
 
     def getPlayerDf(self):
-        df = self.elements_df[['web_name', 'team', 'element_type', 'points_per_game',
-                               'now_cost', 'minutes', 'value_season', 'total_points', 'form', 'value_form']]
+        df = self.elements_df[
+            [
+                "web_name",
+                "team",
+                "element_type",
+                "points_per_game",
+                "now_cost",
+                "minutes",
+                "value_season",
+                "total_points",
+                "form",
+                "value_form",
+            ]
+        ]
 
-        df['element_type'] = df.element_type.map(
-            self.elements_types_df.set_index('id').singular_name)
+        df["element_type"] = df.element_type.map(
+            self.elements_types_df.set_index("id").singular_name
+        )
 
-        df['team'] = df.team.map(
-            self.teams_df.set_index('id').name)
+        df["team"] = df.team.map(self.teams_df.set_index("id").name)
 
         df = self.removeUnwantedTeams(df)
-        df = self.removeLowValues(df)
+        df = self.stringToFloat(df)
 
-        df['value_season_adj'] = df['value_season'] / (df['total_points'] / df['points_per_game'])
+        df["value_season_adj"] = df["value_season"] / (
+            df["total_points"] / df["points_per_game"]
+        )
+
+        df = df[df["now_cost"] < self.max_cost]
+
+        df = self.removeLowValues(df)
 
         return df
 
@@ -36,9 +55,23 @@ class Fantasy:
 
         return df
 
+    def stringToFloat(self, df):
+        columnsShouldBeFloat = [
+            "points_per_game",
+            "now_cost",
+            "minutes",
+            "value_season",
+            "total_points",
+            "form",
+            "value_form",
+        ]
+        for column in columnsShouldBeFloat:
+            df[column] = df[column].astype(float)
+
+        return df
+
     def removeLowValues(self, df):
         for key, value in self.higher_than.items():
-            df[key] = df[key].astype(float)
             df = df.loc[df[key] > value]
 
         return df
@@ -46,8 +79,7 @@ class Fantasy:
     def createPivot(self, index, value):
         df = self.getPlayerDf()
 
-        pivot = df.pivot_table(index=index, values=value,
-                               aggfunc=np.mean).reset_index()
+        pivot = df.pivot_table(index=index, values=value, aggfunc=np.mean).reset_index()
 
         pivot = pivot.sort_values(value, ascending=False)
 
