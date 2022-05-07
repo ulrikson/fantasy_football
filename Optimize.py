91a50ab4772fe9aee20e8049b4d9cd93553167aa
ulrikson <erik.ulrikson@gmail.com>
2022-05-07 19:51:12 +0200

    minor

--- Optimize.py
+++ Optimize.py
@@ -3,9 +3,10 @@ from tabulate import tabulate
 
 
 class Alternative:
-    def __init__(self, player, league):
+    def __init__(self, player, league, in_bank):
         self.player = player
         self.league = league
+        self.in_bank = in_bank * 10
 
         self.df_all = self.__get_base_df()
         self.df_alternatives = self.df_all  # initially
@@ -30,6 +31,7 @@ class Alternative:
         self.__filter_by_cost()
         self.__filter_by_form()
         self.__filter_by_ep()
+        self.__filter_by_value()
 
         self.__print_results()
 
@@ -50,9 +52,10 @@ class Alternative:
 
     def __filter_by_cost(self):
         cost = self.player_stats["now_cost"].iloc[0]
+        cost_threshold = cost + self.in_bank
 
         df = self.df_alternatives
-        df = df[df["now_cost"] <= cost]
+        df = df[df["now_cost"] <= cost_threshold]
 
         self.df_alternatives = df
 
@@ -74,13 +77,39 @@ class Alternative:
 
         self.df_alternatives = df
 
+    def __filter_by_value(self):
+        # value must be around the same as the player
+        value = self.player_stats["value_season_adj"].iloc[0]
+
+        df = self.df_alternatives
+        value_threshold = value * 0.75
+        df = df[df["value_season_adj"] >= value_threshold]
+
+        self.df_alternatives = df
+
     def __print_results(self):
-        headers = ["web_name", "ep_next", "form", "now_cost", "value_season_adj"]
+        headers = [
+            "web_name",
+            "team",
+            "element_type",
+            "ep_next",
+            "form",
+            "now_cost",
+            "value_season_adj",
+        ]
         df = self.df_alternatives[headers]
         df = df.sort_values("ep_next", ascending=False)
         df["now_cost"] = df["now_cost"] / 10
 
-        print(tabulate(df, headers=headers, showindex=False, tablefmt="github"))
+        nr_alternatives = len(df) - 1  # player itself is in the df
+
+        print(f"There are {nr_alternatives} alternatives to {self.player}: \n")
+        print(tabulate(df, headers=headers, showindex=False, tablefmt="psql"))
 
 
-Alternative("Bowen", "fpl").better_choice(same_position=True)
+player = "Salah"
+league = "fpl"
+bank = 0.1
+same_position = True
+
+Alternative(player, league, bank).better_choice(same_position)
