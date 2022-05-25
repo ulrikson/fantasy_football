98410c5cb6aeb89c3c8b4db1a3d06ad20c1b17cf
ulrikson <erik.ulrikson@gmail.com>
2022-05-25 15:31:40 +0200

    refactoring: FDR

--- FDR.py
+++ FDR.py
@@ -1,5 +1,3 @@
-from black import diff
-import requests
 import pandas as pd
 import json
 
@@ -11,12 +9,9 @@ class FDR:
         self.gw = self.__get_gw()
 
     def get_fdr_next(self, team):
-        df = self.fixtures_df[self.fixtures_df["event"] == self.gw]
-        match = df[(df["team_a"] == team) | (df["team_h"] == team)]
-
+        match = self.__get_match(team)
         team_ground = self.__get_team_ground(match)
         is_home = team_ground["home"] == team
-
         opponent = team_ground["away"] if is_home else team_ground["home"]
         difficulty = self.__get_difficulty(opponent, is_home)
 
@@ -24,11 +19,16 @@ class FDR:
 
     def __get_gw(self):
         df = self.fixtures_df
-
         df_where_no_stats = df[df["stats"] == "[]"]
         current_gw = df_where_no_stats["event"].iloc[0]
 
         return current_gw
+
+    def __get_match(self, team):
+        df = self.fixtures_df[self.fixtures_df["event"] == self.gw]
+        match = df[(df["team_a"] == team) | (df["team_h"] == team)]
+
+        return match
 
     def __get_team_ground(self, match):
         return {
@@ -45,7 +45,3 @@ class FDR:
         difficulty = difficulty * 1.5 if not is_home else difficulty
 
         return difficulty
-
-
-FDR("fal").get_fdr_next("IFK Norrköping")
-FDR("fal").get_fdr_next("IF Elfsborg")
